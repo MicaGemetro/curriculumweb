55d119f1adc78d5a2afb45d89bd8dbc10b4713a8
MicaGemetro <125944622+MicaGemetro@users.noreply.github.com>
2023-02-21 01:29:36 -0300

    initial commit

---
+++ comportamientos.js
@@ -0,0 +1,34 @@
+function mostrar(){
+	document.getElementById('datos1').style.display = 'block';
+}
+function ocultar(){
+	document.getElementById('datos1').style.display = 'none';
+}
+
+function mostrar2(){
+	document.getElementById('estudios1').style.display = 'block';
+}
+function ocultar2(){
+	document.getElementById('estudios1').style.display = 'none';
+}
+
+function mostrar3(){
+	document.getElementById('otros1').style.display = 'block';
+}
+function ocultar3(){
+	document.getElementById('otros1').style.display = 'none';
+}
+
+function mostrar4(){
+	document.getElementById('experiencia1').style.display = 'block';
+}
+function ocultar4(){
+	document.getElementById('experiencia1').style.display = 'none';
+}
+
+function mostrar5(){
+	document.getElementById('objetivos1').style.display = 'block';
+}
+function ocultar5(){
+	document.getElementById('objetivos1').style.display = 'none';
+}
